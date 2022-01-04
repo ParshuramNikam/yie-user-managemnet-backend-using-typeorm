585e39a568e2e08f549113769f38823a7a98034c
Parshuram Nikam <parshunikam0612@gmail.com>
2022-01-04 14:35:26 +0530

    hlaf work

--- src/index.ts
+++ src/index.ts
@@ -74,51 +74,6 @@ createConnection()
     const instituteRepo = connection.getRepository(Institute);
     const SchoolRepo = connection.getRepository(School);
 
-    // const user1 = new User();
-    // user1.email = "abcd@gmail.com";
-    // user1.password = "Parshu@123";
-    // user1.username = "ABCD";
-
-    // const profile1 = new Profile();
-    // profile1.fullName = "ABCD";
-    // profile1.address = "aBBBBBBB";
-    // profile1.contactNumber = "2564568975";
-    // profile1.age = 25;
-    // profile1.parentName = "AAA BBB CCC";
-
-    // user1.profile = profile1;
-
-    // const school = new School();
-    // school.name = "bababa";
-    // school.users = [user1]; 
-
-    // user1.school = school;
-
-    // const institute = new Institute();
-    // institute.address = "ajaja";
-    // institute.name = "hshshs";
-    // institute.schools = [school];
-
-    // await profileRepository.save(profile1);
-    // await SchoolRepo.save(school);
-    // await userRepository.save(user1);
-    // await instituteRepo.save(institute);
-
-    // const schoolNew = await SchoolRepo.findOne({ where: { id: 'acbe6497-af89-4111-bbc9-52b86057890c'} });
-
-    // ====================================
-
-    // const instituteOld = await instituteRepo.findOne({ where: {id: 'ff48f44d-c8cf-48d9-a29c-00557999538a'} });
-
-    // const school = new School();
-    // school.name = "AAAAA";
-    // school.institute = instituteOld;
-
-    // await SchoolRepo.save(school);
-
-    // ====================================
-
-
     const user = await userRepository.find({ relations: ['profile', 'school'] });
     const schools = await SchoolRepo.find({ relations: ['institute'] });
     const institutes = await instituteRepo.find({});
